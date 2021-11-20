acbb6d2f66c8219819203ccfc71659575b077045
xianqirui <81338912+xianqirui@users.noreply.github.com>
2021-11-20 12:32:43 +0800

    pull commit

--- src/main/java/com/xqr/git/gittest.java
+++ src/main/java/com/xqr/git/gittest.java
@@ -9,5 +9,6 @@ public class gittest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push到github");
+        System.out.println("pull到本地");
     }
 }
