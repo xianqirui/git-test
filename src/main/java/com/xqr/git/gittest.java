df485560672c861460501ec39b3b2062bbc9aac6
xqr <xqr@xqr.com>
2021-11-20 11:59:12 +0800

    third commit

--- src/main/java/com/xqr/git/gittest.java
+++ src/main/java/com/xqr/git/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("你好,Git");
         System.out.println("你好,Github");
+        System.out.println("你好,Github2");
     }
 }
